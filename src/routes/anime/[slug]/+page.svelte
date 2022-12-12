7d9e9baf8d13da22908762059f0e2099613ba734
sabrina <sabrinaabrodi20@gmail.com>
2022-12-12 19:22:08 +0100

    Fix: fixing layout and styling

--- src/routes/anime/[slug]/+page.svelte
+++ src/routes/anime/[slug]/+page.svelte
@@ -5,11 +5,20 @@
 	console.log('data', data);
 </script>
 
-<section class="flex flex-row">
-	<img class="rounded-xl" src={data.anime.data.images.jpg.image_url} alt={data.anime.data.title} />
-	<div class="">
-		<h2>{data.anime.data.title}</h2>
-		<p>{data.anime.data.synopsis}</p>
-		<p>{data.anime.data.rating}</p>
+<section class="">
+	<div class="flex flex-row place-items-center max-w-6xl mx-auto place-self-center mt-52">
+		<div class="w-1/2">
+			<img
+				class="rounded-xl h-96"
+				src={data.anime.data.images.jpg.image_url}
+				alt={data.anime.data.title}
+			/>
+		</div>
+
+		<div class=" w-1/2">
+			<h2 class="text-3xl text-black uppercase mb-4 font-extrabold">{data.anime.data.title}</h2>
+			<p class="text-gray-800 text-base mb-4">{data.anime.data.synopsis}</p>
+			<p class="font-semibold text-sm text-black italic">{data.anime.data.rating}</p>
+		</div>
 	</div>
 </section>
