9af5c8f2144496c0fdbd342b8ca688bcc7a874ef
sabrina <sabrinaabrodi20@gmail.com>
2022-12-15 20:13:43 +0100

    feat: link back

--- src/routes/anime/[slug]/+page.svelte
+++ src/routes/anime/[slug]/+page.svelte
@@ -6,6 +6,20 @@
 </script>
 
 <section class="">
+	<a class="flex flex-row place-items-center text-xl ml-4 mt-4" href="/">
+		<svg
+			class="text-4xl"
+			xmlns="http://www.w3.org/2000/svg"
+			width="1em"
+			height="1em"
+			preserveAspectRatio="xMidYMid meet"
+			viewBox="0 0 24 24"
+			><path
+				fill="currentColor"
+				d="M14.71 15.88L10.83 12l3.88-3.88a.996.996 0 1 0-1.41-1.41L8.71 11.3a.996.996 0 0 0 0 1.41l4.59 4.59c.39.39 1.02.39 1.41 0c.38-.39.39-1.03 0-1.42z"
+			/></svg
+		> Back</a
+	>
 	<div class="flex flex-row place-items-center max-w-6xl mx-auto place-self-center mt-52">
 		<div class="w-1/2">
 			<img
